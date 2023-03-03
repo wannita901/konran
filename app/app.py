5606cb83b86b8f5f4ba6ed2e42e0e776cbefffb9
kurohima17 <dcha0081@student.monash.edu>
2023-03-04 00:00:38 +1100

    Initialise base template + main flask factory

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "homepage lmao"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
