48413d3739cd62027d2565039788e6c364cac3a6
kurohima17 <dcha0081@student.monash.edu>
2023-03-04 17:50:22 +1100

    home.html now send push request to app.py

--- app/app.py
+++ app/app.py
@@ -1,16 +1,23 @@
 from flask import Flask, request, render_template
 import policy_scrape
-# import wordprocessor
+#import wordprocessor
 
 app = Flask(__name__)
 
 @app.route('/', methods =["GET", "POST"])
 def home():
     if request.method == "POST":
-        url = request.form.get('url')
-        blocks = policy_scrape.scrape_policy(url)
-        return blocks
+        url = request.form['url']
+        print(url)
+        #blocks = policy_scrape.scrape_policy(url)
+        return "hello worldc"
     return render_template('home.html')
+
+@app.route('/get', methods=["POST"])
+def submit_form():
+    url = request.form['url']
+
+    return url
 
 if __name__ == "__main__":
     app.run()
