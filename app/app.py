e171a146b5edd7cc9045fcc8362610b2c5d83437
kurohima17 <dcha0081@student.monash.edu>
2023-03-04 16:39:47 +1100

    Restructure folder for flask

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import policy_scrape
+# import wordprocessor
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods =["GET", "POST"])
 def home():
+    if request.method == "POST":
+        url = request.form.get('url')
+        blocks = policy_scrape.scrape_policy(url)
+        return blocks
     return render_template('home.html')
 
 if __name__ == "__main__":
