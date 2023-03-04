641a20fb389761894580d3279c873c4f84d8a75e
Taylor <58658294+Commanchae@users.noreply.github.com>
2023-03-04 11:58:52 +1100

    policy_scrape.py added, scrapes website for header and text.

--- policy_scrape.py
+++ policy_scrape.py
@@ -3,13 +3,27 @@ import requests
 
 URL = "https://www.twitch.tv/p/en/legal/privacy-notice/"
 URL_APPLE = "https://www.apple.com/legal/privacy/en-ww/"
-page = requests.get(URL)
+URL_SLACK = "https://slack.com/intl/en-au/trust/privacy/privacy-policy"
+page = requests.get(URL_SLACK)
 
 results_test = open("result.txt", "w+", encoding="utf-8")
 
 soup = BeautifulSoup(page.text, "html.parser")
-for item in soup.descendants:
-    if any(tag in repr(item) for tag in ["</p>", "</ul>", "</ol>", "</b>"]):
-        results_test.write(item.text+"\n")
+
+# No Headers Method
+
+'''
+for item in soup.find_all("p"):
+    results_test.write(item.text+"\n\n")
+'''
+
+# With Headers Method
+for element in soup.find_all():
+    if element.name == 'p':
+        results_test.write(element.text + "\n\n")
+    elif element.name in ['h1', 'h2', 'h3', 'h4','h5','h6']:
+        results_test.write("[" + element.text + "]")
+
+
 
 results_test.close()
