4344a17d20cc845bc3a9cbe5e605eedcecb13bdb
kurohima17 <dcha0081@student.monash.edu>
2023-03-04 20:07:32 +1100

    Add /get page to receive url content

--- app/app.py
+++ app/app.py
@@ -4,19 +4,13 @@ import policy_scrape
 
 app = Flask(__name__)
 
-@app.route('/', methods =["GET", "POST"])
+@app.route('/')
 def home():
-    if request.method == "POST":
-        url = request.form['url']
-        print(url)
-        #blocks = policy_scrape.scrape_policy(url)
-        return "hello worldc"
     return render_template('home.html')
 
 @app.route('/get', methods=["POST"])
 def submit_form():
     url = request.form['url']
-
     return url
 
 if __name__ == "__main__":
